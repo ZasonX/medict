f7a94e7961c2b3ad504c3f08a1f0b69b0b482b60
Jason <16561241+ZasonX@users.noreply.github.com>
2026-01-11 04:51:55 +0000

    config eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,12 +1,52 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import tsPlugin from 'typescript-eslint'
+import vuePlugin from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginVue.configs["flat/essential"],
-  { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
-]);
+export default [
+  { ignores: ['dist', 'node_modules', 'wailsjs'] },
+  
+  js.configs.recommended,
+  ...tsPlugin.configs.recommended,
+  ...vuePlugin.configs['flat/recommended'],
+  prettierConfig,
+  
+  {
+    files: ['**/*.{js,ts,vue}'],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module'
+      }
+    }
+  },
+  
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsPlugin.parser,
+        ecmaVersion: 2022,
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      prettier
+    },
+    rules: {
+      'prettier/prettier': 'warn'
+    }
+  },
+  
+  {
+    files: ['**/*.d.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
+  }
+]
